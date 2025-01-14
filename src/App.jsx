e346894f3b3b91b8173d1923626a293d07ddf905
Durjoy96 <mddurjoy853@gmail.com>
2025-01-14 14:00:00 +0600

    initial setup and shadcn ui setup complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <button className="px-5 py-2 rounded-lg border">Default</button>
+      <Button>click me</Button>
+    </div>
+  );
+};
+
+export default App;
