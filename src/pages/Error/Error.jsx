59800231f46a9374ea1fef50335367f20de5b9e8
Durjoy96 <mddurjoy853@gmail.com>
2025-02-19 15:47:27 +0600

    feat: enhance Error page with image and navigation link

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,14 +1,32 @@
 import { useRouteError } from "react-router-dom";
+import ErrorLaptopImg from "../../assets/images/error.png";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   const error = useRouteError();
-  console.error(error);
+
   return (
-    <div>
-      <p>{error.status}</p>
-      <p>{error.statusText}</p>
-      <p>{error.error.message}</p>
-    </div>
+    <>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div className="max-w-32 lg:max-w-64">
+          <img className="w-full animate-bounce" src={ErrorLaptopImg} alt="" />
+        </div>
+        <div className="text-center">
+          <h2 className="text-4xl lg:text-7xl font-bold text-red-500">
+            {error.status}
+          </h2>
+          <p className="text-base font-bold text-red-500">
+            {error.statusText}!
+          </p>
+        </div>
+        <Link
+          to="/"
+          className="mt-8 px-6 py-2 rounded-full border-2 border-primary bg-primary text-white text-base"
+        >
+          Go Home
+        </Link>
+      </div>
+    </>
   );
 };
 
