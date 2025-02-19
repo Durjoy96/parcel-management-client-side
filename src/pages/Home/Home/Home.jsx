e492fb14aa73e6ce14a521807d15b95f510f832a
Durjoy96 <mddurjoy853@gmail.com>
2025-02-19 11:59:16 +0600

    feat: add TrustedBy component to Home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../Banner/Banner";
 import OurFeatures from "../OurFeatures/OurFeatures";
 import TheTopDeliveryMen from "../TheTopDeliveryMen/TheTopDeliveryMen";
 import Faq from "../FAQ/Faq";
+import TrustedBy from "../TrustedBy/TrustedBy";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
         <div>
           <Banner />
         </div>
+        <TrustedBy />
         <div className="mt-12 md:mt-20 lg:mt-32">
           <OurFeatures />
         </div>
